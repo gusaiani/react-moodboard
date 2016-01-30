e2d57e602aeac3f2e930682b488b5dfef4f2674d
Gustavo Saiani <gustavo@poe.ma>
2016-01-30 14:06:15 -0200

    Sanity check: Heroku push

--- client/components/Index/Index.jsx
+++ client/components/Index/Index.jsx
@@ -10,7 +10,7 @@ class IndexComponent extends Component {
 
     return (
       <section>
-        <h2>react-webpack-boilerplate</h2>
+        <h2>Roost in Green</h2>
         <ul ref="indexList" className="index-list">
           {this.props.items.map((item, index) => {
             return (<li key={index}>item {item}</li>);
