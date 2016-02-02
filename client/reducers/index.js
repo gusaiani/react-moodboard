2cb43f6f02471704d0e11323a66cec2498be2c61
Gustavo Saiani <gustavo@poe.ma>
2016-02-02 15:14:07 -0200

    Style buttons: see more and zoom

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
 import { routeReducer } from 'redux-simple-router'
 
+import boardItems from './boardItems'
+
 const rootReducer = combineReducers({
+  boardItems,
   routing: routeReducer
 })
 
